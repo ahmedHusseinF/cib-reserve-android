b2ac5c464b36352fe67daf6e54bc30d4bebbab33
ahmedHusseinF <hussein.ahmed50@gmail.com>
2018-03-27 19:50:53 +0200

    dynamic branches added, yay mariam

--- app/src/main/java/com/cibeg/cibreserve/BranchesActivity.java
+++ app/src/main/java/com/cibeg/cibreserve/BranchesActivity.java
@@ -3,6 +3,7 @@ package com.cibeg.cibreserve;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -10,11 +11,15 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -23,9 +28,11 @@ import java.util.List;
 import java.util.Map;
 
 public class BranchesActivity extends AppCompatActivity {
+    private static String  TAG = "CIB Bank";
     //DataBase Field
     private FirebaseFirestore DataBase;
     ArrayList<String> BankBranches=new ArrayList<>();
+    Spinner s_branches;
 
 
 
@@ -39,9 +46,9 @@ public class BranchesActivity extends AppCompatActivity {
         }catch (Exception e){
             Log.d("Branches", "Can't hide the bar");
         }
-
+        DataBase = FirebaseFirestore.getInstance();
         Spinner s_banks = findViewById(R.id.banks_spinner);
-        Spinner s_branches = findViewById(R.id.branches_spinner);
+        s_branches = findViewById(R.id.branches_spinner);
 
         // Create an ArrayAdapter using the string array and a default spinner layout
 
@@ -58,9 +65,25 @@ public class BranchesActivity extends AppCompatActivity {
         s_banks.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int pos, long l) {
-               BankSelected[0]= adapterView.getItemAtPosition(pos).toString();
 
-                
+               BankSelected[0]= adapterView.getItemAtPosition(pos).toString();
+                DataBase.collection(BankSelected[0])
+                        .get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    for (DocumentSnapshot document : task.getResult()) {
+                                        Log.d(TAG, document.getId() + " => " + document.getData());
+                                        BankBranches.add(document.getId());
+                                    }
+                                    getbranch(BankBranches);
+                                    s_branches.setEnabled(true);
+                                } else {
+                                    Log.d(TAG, "Error getting documents: ", task.getException());
+                                }
+                            }
+                        });
 
                /*
                 DataBase.child("CIB EG").addListenerForSingleValueEvent(new ValueEventListener() {
@@ -106,11 +129,8 @@ public class BranchesActivity extends AppCompatActivity {
                 //BankBranches, android.R.layout.simple_spinner_item);
         // Specify the layout to use when the list of choices appears
         //adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        ArrayAdapter Adapter = new ArrayAdapter(this,
-                android.R.layout.simple_spinner_dropdown_item,
-                BankBranches);
-        s_branches.setEnabled(true);
-        s_branches.setAdapter(Adapter);
+
+
 
         findViewById(R.id.fab).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -121,6 +141,16 @@ public class BranchesActivity extends AppCompatActivity {
                 startActivity(new Intent(BranchesActivity.this, ProcessActivity.class));
             }
         });
+    }
+    public void getbranch(List<String> BankBranches)
+    {
+        ArrayAdapter Adapter = new ArrayAdapter(this,
+                android.R.layout.simple_spinner_dropdown_item,
+                BankBranches);
+        s_branches.setEnabled(true);
+        s_branches.setAdapter(Adapter);
+
+
     }
 
 
